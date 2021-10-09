052ff3651b4a59b1b1a30736642fd521df3ffb44
Adeniyii <ifedayoadeniyi@gmail.com>
2021-10-09 20:26:05 +0100

    feat: setup coffee module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CoffeeController } from './coffee/coffee.controller';
+import { CoffeeModule } from './coffee/coffee.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, CoffeeController],
+  imports: [CoffeeModule],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
